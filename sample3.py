f1dfa1af1452f7176291b387a9daacb0da74a637
arslonov1991 <arslonov.jakhongir.ada@gmail.com>
2026-03-14 17:06:43 +0000

    Commit changes

--- sample3.py
+++ sample3.py
@@ -1,19 +1,19 @@
-# Function for nth Fibonacci number 
+# Function for nth Fibonacci number
 
-def Fibonacci(n): 
-	if n<0: 
-		print("Incorrect input") 
-	# First Fibonacci number is 0 
-	elif n==1: 
-		return 0
-	# Second Fibonacci number is 1 
-	elif n==2: 
-		return 1
-	else: 
-		return Fibonacci(n-1)+Fibonacci(n-2) 
+def fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+        return None
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-# Driver Program 
+# Driver Program
 
-print(Fibonacci(9)) 
+n = 9
+result = fibonacci(n)
 
-#This code is contributed by Saket Modi 
+print(f"Fibonacci number at position {n} is {result}")
